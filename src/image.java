4c2eaa39e30c2e1f25353aa671b7a818c927ad8e
Dave John Coler <fujidave@gmail.com>
2014-09-04 07:41:04 +0700

    Create image.java

---
+++ src/image.java
@@ -0,0 +1,18 @@
+/**
+This will be a class for creating loading and disposing of images
+*/
+
+public class Final511 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        gui g1 = new gui();
+        
+        
+        
+    }
+}
