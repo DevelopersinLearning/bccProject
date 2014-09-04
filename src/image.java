ce89b75275e54f79dabe7514bc48744334c23413
Dave John Coler <fujidave@gmail.com>
2014-09-04 07:42:01 +0700

    Update image.java

--- src/image.java
+++ src/image.java
@@ -2,17 +2,10 @@
 This will be a class for creating loading and disposing of images
 */
 
-public class Final511 {
+public class image {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        gui g1 = new gui();
-        
-        
-        
-    }
+    
 }
